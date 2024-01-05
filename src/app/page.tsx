881c7844ed72300bea13ef106b886a9d634cfddc
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-01-05 20:13:03 -0300

    main and about me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,38 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Main Page
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-center">
+      <header className="mb-5 text-4xl font-bold leading-relaxed pb-10">
+        <h1>
+          <strong className="text-6xl ">
+            Hi, my name is <em className="text-6xl text-[#5c62ec]">Joaquin</em>
+          </strong>
+          <br />a frontend developer
+        </h1>
+        <div className="text-inherit text-lg">
+          <p>improving my skills with no limit</p>
+        </div>
+        <a className="py-3 px-7 bg-[#5c62ec] text-base rounded-md" href="#!">
+          Download CV
+        </a>
+      </header>
+      <section className="bg-white w-screen flex flex-col text-black">
+        <div>
+          <h2 className="py-4 text-3xl font-bold mx-3">About ME</h2> y foto
+        </div>
+        <p className="m-8">
+          👨‍🎓 Young student passionate about Full Stack Web Programming, driven
+          by an insatiable thirst for knowledge and challenges. My versatility,
+          commitment and constant search for quality define my approach to work.
+          Strengths that I consider notable in me: <br />
+          📚 Hunger for knowledge: I always seek to learn and improve, staying
+          updated on technologies and trends. <br />
+          🤝 Team adaptability: My versatility allows me to collaborate
+          effectively with different personalities and roles. <br /> 💪
+          Determination and ambition: My commitment and loyalty to the goals I
+          set drive my potential.
+        </p>
+      </section>
     </main>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <header className="flex justify-between primary">
-      <div>Joaquin Acosta</div>
-      <div>Botones</div>
-    </header>
+    <nav className="flex justify-between primary items-center">
+      <div>
+        <h1 className="bold text-xl py-2 px-4">Joaquin Acosta</h1>
+      </div>
+      <div className="flex gap-2 px-2">
+        <button>Home</button>
+        <button>Projects</button>
+        <button>Contact</button>
+      </div>
+    </nav>
   )
 }
 
