5df53bc0920df26bb551657570c6022df3d511bd
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-01-05 17:11:22 -0300

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className="flex justify-between primary">
+      <div>Joaquin Acosta</div>
+      <div>Botones</div>
+    </header>
+  )
+}
+
+export default Navbar
