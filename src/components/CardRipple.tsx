22b85721297818d7c8057bd9fdf28b63e41ace84
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-01-25 21:47:45 -0300

    responsivity

--- src/components/CardRipple.tsx
+++ src/components/CardRipple.tsx
@@ -26,8 +26,8 @@ const CardRipple = ({
   date,
 }: ProjectProps) => {
   return (
-    <div className="flex w-[1200px] h-[320px] rounded-lg bg-white dark:bg-neutral-700 border">
-      <div className="w-[380px] overflow-hidden bg-cover bg-no-repeat">
+    <div className="block md:flex md:w-[1200px] md:h-[320px] rounded-lg bg-white dark:bg-neutral-700 border">
+      <div className="md:w-[380px] overflow-hidden bg-cover bg-no-repeat">
         <Image
           className="rounded-t-lg h-full object-cover"
           src={`${isProd ? '/joaquindev' : ''}${image}`}
@@ -37,7 +37,7 @@ const CardRipple = ({
         />
         {/* <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div> */}
       </div>
-      <div className="p-6 w-[700px]">
+      <div className="p-6 md:w-[700px]">
         <time className="text-sm">{date}</time>
         <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
           {title}
